6cf7333b8e762905567554e33e3dbfbd612d2599
Zuri Cafe <161525963+NikilSacknut@users.noreply.github.com>
2025-03-07 10:45:31 +1300

    Added  GUI

--- OrderingProgram.py
+++ OrderingProgram.py
@@ -1,5 +1,14 @@
-import tkinter as tkinter
+import tkinter as tk
 
 root = tk.Tk()
+
+root.geometry("800x500")
+root.title("My first GUI")
+
+label = tk.Label(root, text="Hello World", font=('Arial', 18))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, font=('Arial', 16))
+textbox.pack()
 
 root.mainloop()
