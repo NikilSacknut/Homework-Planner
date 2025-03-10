dcf6d19cf4e950fb8658801d380188ddb726e286
Zuri Cafe <161525963+NikilSacknut@users.noreply.github.com>
2025-03-10 14:55:09 +1300

    Added Buttons

--- OrderingProgram.py
+++ OrderingProgram.py
@@ -9,9 +9,9 @@ label = tk.Label(root, text="Hello World", font=('Arial', 18))
 label.pack(padx=20, pady=20)
 
 textbox = tk.Text(root, height=3, font=('Arial', 16))
-textbox.pack()
+textbox.pack(padx=10, pady=10)
 
-myentry =tk.Entry(root)
-myentry.pack()
+button = tk.Button(root, text="Click Me!", font=('Arial', 18))
+button.pack(padx =10, pady=10)
 
 root.mainloop()
