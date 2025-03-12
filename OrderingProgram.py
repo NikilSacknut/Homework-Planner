c9bdd74cfcb3d8cb494f9d9d1908b8cb21cce282
Sccoobs <zuricafe08@gmail.com>
2025-03-13 11:41:45 +1300

    Made Code Neater (Version 1.2)

--- OrderingProgram.py
+++ OrderingProgram.py
@@ -1,11 +1,54 @@
 import tkinter as tk
 from tkinter import ttk
 
+def main():
+    app = Application()
+    app.mainloop()
+
+
 class Application(tk.Tk): 
     def __init__(self):
         super().__init__()
         self.title("Rosmini Homework Planner")
-     
-app = Application()
-app.mainloop()
+        
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=3)
+        self.rowconfigure(0, weight=1) 
 
+        frame = InputForm(self) #Container for window 
+        frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+
+        frame2 = InputForm(self) #Container for window 
+        frame2.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
+
+        
+
+class InputForm(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+
+        self.entry = ttk.Entry(self) #pass onto frame which includes it
+        self.entry.grid(row=0, column=0, sticky="ew")
+
+        self.entry.bind("<Return>", self.add_to_list) #Call back and addes keyboard function
+
+        self.entry_btn = ttk.Button(self, text="Add", command=self.add_to_list)
+        self.entry_btn.grid(row=0, column=1)  
+
+        self.text_list = tk.Listbox(self)
+        self.text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
+
+    def add_to_list(self, event=None):
+        text = self.entry.get()
+        if text:
+            self.text_list.insert(tk.END, text)
+            self.entry.delete(0, tk.END)
+
+
+if __name__ == "__main__":
+    main()
+
+    
