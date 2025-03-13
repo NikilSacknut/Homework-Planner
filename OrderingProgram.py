6efdbee878d711bfea814255556f32a596ecb20b
Zuri Cafe <161525963+NikilSacknut@users.noreply.github.com>
2025-03-14 10:35:20 +1300

    Started on login

--- OrderingProgram.py
+++ OrderingProgram.py
@@ -1,6 +1,62 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 import sqlite3
+
+
+def register():
+    screen1 = Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+
+    username = StringVar()
+    password = StringVar()
+
+    Label(text = "Username * ").pack() 
+    Entry(textvariable = username)
+    Label(text = "Password * ").pack() 
+    Entry(textvariable = password)
+
+
+def login():
+    print("Login session started")
+
+
+def main_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("300x250")
+    screen.title("Rosmini Homework Planner 2025")
+    Label(text = "Rosmini Homework Planner", bg="grey", width="300", height="2", font = ("Calibri", 13)).pack()
+    Label(text = "").pack()
+    Button(text = "Login", height="2", width="30", command="login").pack()
+    Label(text = "").pack() 
+    Button(text = "Register", height = "2", width = "30", command="register").pack()
+
+    screen.mainloop()
+
+main_screen()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
